fd138a7301cfccd0daf4f3bf3d04477ce38635f3
Shiv Rai <sshaktirai@gmail.com>
2024-12-14 16:02:19 +0530

    fixing server

--- server/index.js
+++ server/index.js
@@ -277,7 +277,14 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.url}`);
+  res.setHeader("Access-Control-Allow-Origin", "https://adventurers-arena.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
